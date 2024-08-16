2942d442d05a774789bad583a2095142fa324fb9
lankesh renuse <162421234+lankesh-renuse@users.noreply.github.com>
2024-08-16 12:54:37 +0530

    Add files via upload

---
+++ SpringHibernate/src/main/java/com/springjpa/dao/CityDAO.java
@@ -0,0 +1,26 @@
+package com.springjpa.dao;
+
+import com.springjpa.dto.City;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CityDAO {
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public City getCityById(int id) {
+		return entityManager.find(City.class, id);
+	}
+
+	public List<City> getCitiesLimit(int limit) {
+		return entityManager.createQuery("from City", City.class).setMaxResults(limit).getResultList();
+	}
+
+	public List<City> getCities() {
+		return entityManager.createQuery("from City", City.class).getResultList();
+	}
+}
